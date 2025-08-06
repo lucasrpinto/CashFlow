b970330590e5d28cd436aa6e47d9e3da3205eee8
lucasrpinto <lucasrop22@gmail.com>
2025-08-06 15:42:05 -0300

    Add project files.

---
+++ src/CashFlow.Communication/Requests/RequestRegisterExpenseJson.cs
@@ -0,0 +1,11 @@
+﻿using CashFlow.Communication.Enum;
+
+namespace CashFlow.Communication.Requests;
+public class RequestRegisterExpenseJson
+{
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty ;
+    public DateTime Date { get; set; }
+    public decimal Amount { get; set; }
+    public PaymentType paymentType { get; set; }
+}
