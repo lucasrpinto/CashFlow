d3ed5b1099a02cb3f5f2a3784a86359293c511be
lucasrpinto <lucasrop22@gmail.com>
2025-08-15 16:32:58 -0300

    test: add create endpoint and exception handling tests

---
+++ tests/Validators.Tests/Expenses/Register/RegisterExpenseValidatorTests.cs
@@ -0,0 +1,91 @@
+﻿using CashFlow.Application.UseCases.Expenses.Register;
+using CashFlow.Communication.Enum;
+using CashFlow.Exception;
+using CommonTestUtilities.Requests;
+using FluentAssertions;
+
+namespace Validators.Tests.Expenses.Register;
+public class RegisterExpenseValidatorTests
+{
+    [Fact]
+    public void Sucess()
+    {
+        //Arrange -  Configura o cenário e os dados de entrada para o teste
+        var validator = new RegisterExpenseValidator();
+        var request = RequestRegisterExpenseJsonBuilder.Build();
+
+        //Act -  Executa a ação a ser testada (validação do request)
+        var result = validator.Validate(request);
+
+        //Assert - Verifica se o resultado corresponde ao esperado
+        result.IsValid.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Error_Title_Empty()
+    {
+        //Arrange -  Configura o cenário e os dados de entrada para o teste
+        var validator = new RegisterExpenseValidator();
+        var request = RequestRegisterExpenseJsonBuilder.Build();
+        request.Title = string.Empty;
+
+        //Act -  Executa a ação a ser testada (validação do request)
+        var result = validator.Validate(request);
+
+        //Assert - Verifica se o resultado corresponde ao esperado
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle().And.Contain(e => e.ErrorMessage.Equals(ResourceErrorMessages.TITLE_REQUIRED));
+    }
+
+    [Fact]
+    public void Error_Date_Future()
+    {
+        //Arrange -  Configura o cenário e os dados de entrada para o teste
+        var validator = new RegisterExpenseValidator();
+        var request = RequestRegisterExpenseJsonBuilder.Build();
+        request.Date = DateTime.UtcNow.AddDays(1);
+
+        //Act -  Executa a ação a ser testada (validação do request)
+        var result = validator.Validate(request);
+
+        //Assert - Verifica se o resultado corresponde ao esperado
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle().And.Contain(e => e.ErrorMessage.Equals(ResourceErrorMessages.EXPENSE_CANNOT_FOR_THE_FUTURE));
+    }
+
+    [Fact]
+    public void Error_Payment_Type_Invalid()
+    {
+        //Arrange -  Configura o cenário e os dados de entrada para o teste
+        var validator = new RegisterExpenseValidator();
+        var request = RequestRegisterExpenseJsonBuilder.Build();
+        request.PaymentType = (PaymentType)700;
+
+        //Act -  Executa a ação a ser testada (validação do request)
+        var result = validator.Validate(request);
+
+        //Assert - Verifica se o resultado corresponde ao esperado
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle().And.Contain(e => e.ErrorMessage.Equals(ResourceErrorMessages.PAYMENT_TYPE_INVALID));
+    }
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(-1)]
+    [InlineData(-2)]
+    [InlineData(-7)]
+    public void Error_Amount_Invalid(decimal amount)
+    {
+        //Arrange -  Configura o cenário e os dados de entrada para o teste
+        var validator = new RegisterExpenseValidator();
+        var request = RequestRegisterExpenseJsonBuilder.Build();
+        request.Amount = amount;
+
+        //Act -  Executa a ação a ser testada (validação do request)
+        var result = validator.Validate(request);
+
+        //Assert - Verifica se o resultado corresponde ao esperado
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle().And.Contain(e => e.ErrorMessage.Equals(ResourceErrorMessages.AMOUNT_MUST_BE_GREATER_THAN_ZERO));
+    }
+}
