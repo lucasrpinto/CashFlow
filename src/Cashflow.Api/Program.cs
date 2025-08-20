1ca0ca1a2a7ed624dc3d97d9391a7000120c53aa
lucasrpinto <lucasrop22@gmail.com>
2025-08-20 17:15:08 -0300

    ajust

--- src/Cashflow.Api/Program.cs
+++ src/Cashflow.Api/Program.cs
@@ -15,11 +15,11 @@ builder.Services.AddApplication();
 
 var app = builder.Build();
 
-/*if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-}*/
+}
 
 app.UseHttpsRedirection();
 
